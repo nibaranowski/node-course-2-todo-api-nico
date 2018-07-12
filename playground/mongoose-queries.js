dfaf4031817d8950a8f657cdc2ae1f9c74a98a1b
Nicolas Baranowski <nibaranowski@gmail.com>
2018-07-12 12:58:44 +0200

    add mongoose queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,47 @@
+// CRUD queries with mongooseconst
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+
+// var id = '5b471faeb12c0c11d543b37a1';
+//
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// }
+// //
+// // Todo.find({
+// //     _id: id
+// // }).then((todos) => {
+// //     console.log('Todos', todos);
+// // });
+// //
+// // Todo.findOne({
+// //     _id: id
+// // }).then((todo) => {
+// //     console.log('Todo', todo);
+// // });
+//
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found')
+//     }
+//     console.log('Todo By Id', todo)
+// }).catch((e) => console.log(e));
+
+
+var id = '6b462996a871510ce531adbb';
+
+if (!ObjectID.isValid(id)) {
+    console.log('ID not valid');
+}
+
+
+User.findById(id).then((user) => {
+    if (!user) {
+        return console.log('Unable to find user')
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+}).catch((e) => console.log(e));
