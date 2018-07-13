526614d3acd35cd128a8aa4215bf954e3228bb69
Nicolas Baranowski <nibaranowski@gmail.com>
2018-07-13 14:09:38 +0200

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -40,7 +40,7 @@ UserSchema.methods.toJSON = function () {
 };
 
 UserSchema.methods.generateAuthToken = function () {
-    var user = this;
+    var user = this; //instance method (call instance)
     var access = 'auth';
     var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
@@ -48,6 +48,22 @@ UserSchema.methods.generateAuthToken = function () {
 
     return user.save().then(() => {
         return token;
+    });
+};
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this; //model method (call model)
+    var decoded;
+
+    try {
+        decoded  = jwt.verify(token, 'abc123')
+    } catch (e) {
+        return Promise.reject();
+    }
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 };
 
